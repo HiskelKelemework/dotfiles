efed97db43328b0f9be98a6f10574889875d7576
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2025-12-17 16:16:43 +0300

    fix: make lua ls support nvim api autocompletion

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -14,7 +14,18 @@ return {
       runtime = {
         version = 'LuaJIT',
       },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }
-
